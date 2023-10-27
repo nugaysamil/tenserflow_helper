3b1e81bbaeb765ad2444198b860c6173869734a4
Şamil Nugay <samil_nugay@hotmail.com>
2023-10-27 16:45:08 +0300

    sealed instead of final

--- lib/src/task/bindings/text/nl_classifier/types.dart
+++ lib/src/task/bindings/text/nl_classifier/types.dart
@@ -2,9 +2,9 @@ import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 
-final class TfLiteNLClassifier extends Opaque {}
+ sealed class TfLiteNLClassifier extends Opaque {}
 
-final class TfLiteNLClassifierOptions extends Struct {
+ sealed class TfLiteNLClassifierOptions extends Struct {
   @Int32()
   external int inputTensorIndex;
 
@@ -40,23 +40,23 @@ final class TfLiteNLClassifierOptions extends Struct {
   }
 }
 
-final class TfLiteCategories extends Struct {
+ sealed class TfLiteCategories extends Struct {
   @Int32()
   external int size;
 
   external Pointer<TfLiteCategory> categories;
 }
 
-final class TfLiteCategory extends Struct {
+ sealed class TfLiteCategory extends Struct {
   external Pointer<Utf8> text;
 
   @Double()
   external double score;
 }
 
-final class TfLiteBertNLClassifier extends Opaque {}
+ sealed class TfLiteBertNLClassifier extends Opaque {}
 
-final class TfLiteBertNLClassifierOptions extends Struct {
+ sealed class TfLiteBertNLClassifierOptions extends Struct {
   @Int32()
   external int maxSeqLen;
 
--- lib/src/task/bindings/text/qa/types.dart
+++ lib/src/task/bindings/text/qa/types.dart
@@ -2,9 +2,9 @@ import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 
-final class TfLiteBertQuestionAnswerer extends Opaque {}
+sealed class TfLiteBertQuestionAnswerer extends Opaque {}
 
-final class TfLiteQaAnswer extends Struct {
+sealed class TfLiteQaAnswer extends Struct {
   @Int32()
   external int start;
   @Int32()
@@ -15,7 +15,7 @@ final class TfLiteQaAnswer extends Struct {
   external Pointer<Utf8> text;
 }
 
-final class TfLiteQaAnswers extends Struct {
+sealed class TfLiteQaAnswers extends Struct {
   @Int32()
   external int size;
 
